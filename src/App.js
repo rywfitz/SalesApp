8d00598220d98f7f65e3b0ea9f2d5b3245c0b9fc
Trevor Davis <tjdavis2112@gmail.com>
2022-06-07 17:59:54 -0400

    test 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import MovieCard from "./Moviecard";
+import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
